0f2f96fc97a7fb7ff4a84b49d8b56454bf4c1ec1
Yusuf-Solmaz <yusufmucahitsolmaz@gmail.com>
2023-08-29 15:22:36 +0300

    DTO to Model Functions

--- app/src/main/java/com/yusuf/moviesearch/data/remote/dto/MovieDetailDto.kt
+++ app/src/main/java/com/yusuf/moviesearch/data/remote/dto/MovieDetailDto.kt
@@ -1,5 +1,7 @@
 package com.yusuf.moviesearch.data.remote.dto
 
+import com.yusuf.moviesearch.domain.model.MovieDetail
+
 data class MovieDetailDto(
     val Actors: String,
     val Awards: String,
@@ -27,3 +29,9 @@ data class MovieDetailDto(
     val imdbRating: String,
     val imdbVotes: String
 )
+
+fun MovieDetailDto.toMovieDetail() : MovieDetail {
+    return MovieDetail(
+        Actors,Awards,Country,Director,Genre,Language,Poster,Rated,Released,Title,Type,Year,imdbRating
+    )
+}
--- app/src/main/java/com/yusuf/moviesearch/data/remote/dto/MoviesDto.kt
+++ app/src/main/java/com/yusuf/moviesearch/data/remote/dto/MoviesDto.kt
@@ -1,7 +1,13 @@
 package com.yusuf.moviesearch.data.remote.dto
 
+import com.yusuf.moviesearch.domain.model.Movie
+
 data class MoviesDto(
     val Response: String,
     val Search: List<Search>,
     val totalResults: String
 )
+
+fun MoviesDto.toMovieList(): List<Movie>{
+    return Search.map { search -> Movie(search.Poster,search.Title,search.Year,search.imdbID) }
+}
