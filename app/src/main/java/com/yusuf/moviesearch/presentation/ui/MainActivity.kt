dc179d79866da7845ee7c6f76dd9f6b464351dbb
Yusuf-Solmaz <yusufmucahitsolmaz@gmail.com>
2023-08-29 11:27:59 +0300

    Location Update

--- app/src/main/java/com/yusuf/moviesearch/presentation/ui/MainActivity.kt
+++ app/src/main/java/com/yusuf/moviesearch/presentation/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.yusuf.moviesearch
+package com.yusuf.moviesearch.presentation.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.yusuf.moviesearch.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
