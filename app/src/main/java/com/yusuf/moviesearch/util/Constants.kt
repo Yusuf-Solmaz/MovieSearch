4ffa913e499c301fb6f49012ccaa693053c0cb9b
Yusuf-Solmaz <yusufmucahitsolmaz@gmail.com>
2023-08-29 14:08:54 +0300

    Constants

---
+++ app/src/main/java/com/yusuf/moviesearch/util/Constants.kt
@@ -0,0 +1,11 @@
+package com.yusuf.moviesearch.util
+
+object Constants {
+
+    const val API_KEY= "95a2bdea"
+
+    const val BASE_URL = "https://www.omdbapi.com"
+
+    const val IMBD_ID = "imdb_id"
+
+}
