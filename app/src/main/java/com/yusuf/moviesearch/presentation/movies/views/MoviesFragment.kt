4baca903e8daeb548e897bd0f92be1229c0b973e
Yusuf-Solmaz <yusufmucahitsolmaz@gmail.com>
2023-08-30 15:52:22 +0300

    Movie Detail and Movie Fragment

---
+++ app/src/main/java/com/yusuf/moviesearch/presentation/movies/views/MoviesFragment.kt
@@ -0,0 +1,30 @@
+package com.yusuf.moviesearch.presentation.movies.views
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.yusuf.moviesearch.R
+
+class MoviesFragment : Fragment() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_movies, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+}
