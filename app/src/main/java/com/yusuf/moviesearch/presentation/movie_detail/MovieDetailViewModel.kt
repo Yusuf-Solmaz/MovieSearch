70a893afbe3f865880302ed1038af069d9bebf16
Yusuf-Solmaz <yusufmucahitsolmaz@gmail.com>
2023-09-04 14:21:38 +0300

    Movie View Model Methods, Update

--- app/src/main/java/com/yusuf/moviesearch/presentation/movie_detail/MovieDetailViewModel.kt
+++ app/src/main/java/com/yusuf/moviesearch/presentation/movie_detail/MovieDetailViewModel.kt
@@ -1,46 +1,39 @@
-package com.yusuf.moviesearch.presentation.movie_detail
+// MovieDetailViewModel.kt
 
+package com.yusuf.moviesearch.presentation.movie_detail
 
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.yusuf.moviesearch.domain.use_case.get_movie_detail.GetMovieDetailUseCase
-import com.yusuf.moviesearch.presentation.movies.MoviesEvent
 import com.yusuf.moviesearch.util.Resource
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.Job
-import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
 import javax.inject.Inject
-import com.yusuf.moviesearch.util.Constants.IMBD_ID
 
 @HiltViewModel
 class MovieDetailViewModel @Inject constructor(
     private val getMovieDetailsUseCase: GetMovieDetailUseCase,
     private val stateHandle: SavedStateHandle
-): ViewModel() {
-
-
+) : ViewModel() {
 
     private val _state = MutableStateFlow<MovieDetailState>(MovieDetailState())
     val state: StateFlow<MovieDetailState> = _state
 
-    init {
-        stateHandle.get<String>(IMBD_ID)?.let {
-            getMovieDetail(it)
-        }
+    fun setImdbId(imdbId: String) {
+        getMovieDetail(imdbId)
     }
 
-    private fun getMovieDetail(imdbId: String){
+    private fun getMovieDetail(imdbId: String) {
         getMovieDetailsUseCase.executeGetMovieDetail(imdb = imdbId).onEach {
-            when(it){
-                is Resource.Success ->{
+            when (it) {
+                is Resource.Success -> {
                     _state.value = MovieDetailState(movie = it.data)
                 }
-                is Resource.Error ->{
+                is Resource.Error -> {
                     _state.value = MovieDetailState(error = it.message ?: "Error!")
                 }
                 is Resource.Loading -> {
@@ -48,18 +41,5 @@ class MovieDetailViewModel @Inject constructor(
                 }
             }
         }.launchIn(viewModelScope)
-    }
-
-    fun onEvent(event: MoviesEvent) {
-
-        when (event) {
-
-            is MoviesEvent.Search -> {
-
-                getMovieDetail(event.search)
-
-            }
-
-        }
     }
 }
